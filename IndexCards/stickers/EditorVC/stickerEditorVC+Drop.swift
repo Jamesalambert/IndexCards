a0e64502a7458d87dd30536852973e83dcc44762
James Lambert <james.a.lambert@gmail.com>
2020-08-12 00:25:49 +0100

    drop code improvements continue!

--- IndexCards/stickers/EditorVC/stickerEditorVC+Drop.swift
+++ IndexCards/stickers/EditorVC/stickerEditorVC+Drop.swift
@@ -38,76 +38,91 @@ UIDropInteractionDelegate
     }
     
     
-    
+
     func dropInteraction(_ interaction: UIDropInteraction,
                 performDrop session: UIDropSession) {
         
         let dropPoint = session.location(in: stickerView)
         
-        
         for item in session.items{
             
             //dropping stickers locally
             if let stickerKind = item.localObject as? StickerKind{
-                let _ = self.addDroppedShape(shape: stickerKind,
-                atLocation: dropPoint)
-                
+                let _ = self.addDroppedShape(   shape:      stickerKind,
+                                                atLocation: dropPoint)
                 continue
             }
+            
+            //drop images???
  
             //dropping URLS
             if item.itemProvider.canLoadObject(ofClass: NSURL.self){
-                
-                item.itemProvider.loadObject(ofClass: NSURL.self,
-                                             completionHandler:
-                    
-                    {[weak self] (provider, error) in
-                    
-                    DispatchQueue.main.async {
-                        if error == nil {
-                            if let draggedURL = (provider as? NSURL){
-                                
-                                let newStickerText = NSAttributedString(
-                                    string: draggedURL.absoluteString ?? "",
-                                    attributes:
-                                    [NSAttributedString.Key.link : draggedURL.absoluteString ?? "" ])
-                                
-                                let newSticker = self?.addDroppedShape(shape: .RoundRect,
-                                                                       atLocation: dropPoint)
-                                
-                                if let newSticker = newSticker as? WritingSticker{
-                                    newSticker.stickerAttributedText = newStickerText
-                                }
-                                
-                            }
-                        } //if
-                    } //dispatch
-                    
-                })
-                
+                let _ = loadDroppedURLS(item, dropPoint)
                 continue  //so we don't try to load a url as text below
-            } //if
-           
-            
-            
+            }
+
             //dropping strings
-            item.itemProvider.loadObject(ofClass: NSAttributedString.self, completionHandler: {
-                [weak self] (provider, error) in
-                
-                DispatchQueue.main.async {
-                    if error == nil {
-                        if let draggedString = provider as? NSAttributedString {
-                            
-                            let newSticker = self?.addDroppedShape(shape: .RoundRect,
-                                                                  atLocation: dropPoint)
-                            newSticker?.stickerText = draggedString.string
-                        }
-                    } //if
-                } //dispatch
-            })
-            
-            
-        } //for items
+            if item.itemProvider.canLoadObject(ofClass: NSAttributedString.self){
+                let _ = loadDroppedText(item, dropPoint)
+            }
+        }
         
-    } // func
+    }
+    
+    
+    //MARK:- private methods
+    
+    fileprivate func loadDroppedURLS(_ item: UIDragItem, _ dropPoint: CGPoint) -> Progress {
+         
+         return  item
+                 .itemProvider
+                 .loadObject(    ofClass: NSURL.self,
+                                 completionHandler:
+             
+                 {[weak self] (provider, error) in
+                     
+                     DispatchQueue.main.async{
+                         
+                         guard error == nil else {return}
+                         guard let draggedURL = (provider as? NSURL) else {return}
+                             
+                         let newStickerText = NSAttributedString(
+                             string: draggedURL.absoluteString ?? "",
+                             attributes:
+                             [NSAttributedString.Key.link : draggedURL.absoluteString ?? "" ])
+                         
+                         let newSticker = self?.addDroppedShape(shape: .RoundRect,
+                                                                atLocation: dropPoint)
+                         
+                         if let newSticker = newSticker as? WritingSticker{
+                             newSticker.stickerAttributedText = newStickerText
+                         }
+                     }
+                 })
+     }
+     
+     fileprivate func loadDroppedText(_ item: UIDragItem, _ dropPoint: CGPoint) -> Progress {
+         
+         return  item
+                 .itemProvider
+                 .loadObject(    ofClass: NSAttributedString.self,
+                                 completionHandler:
+                     
+                     {[weak self] (provider, error) in
+                     
+                         DispatchQueue.main.async {
+                             guard error == nil else {return}
+                             guard let draggedString = provider as? NSAttributedString else {return}
+                             
+                             let newSticker = self?.addDroppedShape(shape: .RoundRect,
+                                                                    atLocation: dropPoint)
+                             newSticker?.stickerText = draggedString.string
+                         }
+                     })
+     }
+    
+    
+    
+    
+    
 }
